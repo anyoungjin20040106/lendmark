f920be0d3cb5fe942fdaaea4236843ff0065365c
안영진 <anyoungjin0106@kbu.ac.kr>
2024-08-27 10:42:10 +0900

    create map

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form,Query
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import folium
+from geopy import distance
 
 lendmarkdf = pd.read_csv('lendmark.csv')
 app = FastAPI()
@@ -15,33 +18,19 @@ async def content(title: str = Form(...)):
     item = pd.read_csv('item.csv')
     return item[item['이름'] == title]['설명'].values[0]
 
-
-@app.post("/lendmark")
-async def lendmark(kind: str = Form(...)):
+@app.get("/map")
+async def map(lat:str=Query(...),lon:str=Query(...),kind:str=Query("모두보기"),km:str=Query(4)):
+    m = folium.Map(location=[lat,lon], zoom_start=12)
     tf = lendmarkdf[lendmarkdf['종류'] ==
-                    kind] if kind != "모두보기" else lendmarkdf.copy()
-    return {
-        'data': [{
-            "name": row[0],
-            "lat": row[1],
-            "lon": row[2],
-            "img": row[4],
-            "content": row[5],
-        } for row in tf.values]
-    }
-
-
-@app.get("/lendmark")
-async def lendmark():
-    return {
-        'data': [{
-            "name": row[0],
-            "lat": row[1],
-            "lon": row[2],
-            "img": row[4],
-            "content": row[5],
-        } for row in lendmarkdf.values]
-    }
+                    kind].copy() if kind != "모두보기" else lendmarkdf.copy()
+    tf['거리'] = tf.apply(lambda x: distance.distance((x['위도'], x['경도']), (lat, lon)).km, axis=1)
+    tf=tf[tf['거리']<=float(km)]
+    folium.Marker([lat,lon]).add_to(m)
+    for row in tf.values:
+        folium.Marker(location=[row[1],row[2]],icon=folium.Icon(color='red'),popup=f"""
+                      <h1>{row[0]}</h1><dialog id={row[0]}><form method="dialog"><h1>{row[0]}</h1><img src="{row[4]}" width="100%"><br><h2>{row[5]}</h2><h2>거리 : {row[6]}</h2><button type="button"><h2>ar로 보기</h2></button><button><h2>닫기</h2></button></form></dialog><button onclick="document.getElementById('{row[0]}').showModal();">설명보기</button>""").add_to(m)
+    map_html = m._repr_html_()
+    return HTMLResponse(content=map_html)
 
 
 @app.get("/kind")
