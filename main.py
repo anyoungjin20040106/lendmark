6f5a66e7baf7995aa2c0fdc007b74b0c7595039d
안영진 <anyoungjin0106@kbu.ac.kr>
2024-09-23 18:33:43 +0900

    create item.json

--- main.py
+++ main.py
@@ -1,60 +1,40 @@
 import pandas as pd
 from fastapi import FastAPI, Form,Query
-from fastapi.responses import HTMLResponse,FileResponse
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import folium
 from geopy import distance
 
 lendmarkdf = pd.read_csv('lendmark.csv')
-navi = pd.read_csv('navi.csv')
 app = FastAPI()
+app.mount("/img",StaticFiles(directory="image"))
+df=pd.read_json('data.json')
 lendmarkdf.fillna(
     'https://maps.gstatic.com/tactile/pane/default_geocode-2x.png',
     inplace=True)
 templates = Jinja2Templates(directory="template")
 
-@app.get("/")
-async def index():
-    return FileResponse("index.html")
 @app.post("/content")
 async def content(title: str = Form(...)):
     item = pd.read_csv('item.csv')
     return item[item['이름'] == title]['설명'].values[0]
 
-@app.post("/map")
-async def map(lat:str=Form(...),lon:str=Form(...),km:str=Form(4)):
+@app.get("/map")
+async def map(lat:str=Query(...),lon:str=Query(...)):
     m = folium.Map(location=[lat,lon], zoom_start=12)
     tf = lendmarkdf.copy()
     folium.Marker([lat,lon]).add_to(m)
-    folium.CircleMarker([lat,lon],radius=km*100).add_to(m)
     for row in tf.values:
         folium.Marker(location=[row[1],row[2]],icon=folium.Icon(color='red'),popup=f"""
-                      <h1>{row[0]}</h1><dialog id={row[0]}><form method="dialog"><h1>{row[0]}</h1><img src="{row[4]}" width="100%"><br><h2>{row[5]}</h2><h2>거리 : {row[6]}</h2><button type="button"><h2>ar로 보기</h2></button><button><h2>닫기</h2></button></form></dialog><button onclick="document.getElementById('{row[0]}').showModal();">설명보기</button>""").add_to(m)
+                      <h1>{row[0]}</h1><dialog id={row[0]}><form method="dialog"><h1>{row[0]}</h1><img src="{row[4]}" width="100%"><br><h2>{row[5]}</h2><button type="button"><h2>ar로 보기</h2></button><button><h2>닫기</h2></button></form></dialog><button onclick="document.getElementById('{row[0]}').showModal();">설명보기</button>""").add_to(m)
     map_html = m._repr_html_()
     return HTMLResponse(content=map_html)
-
-
 @app.get("/kind")
 async def kind():
     return {'data': ['모두보기'] + list(lendmarkdf['종류'].unique())}
-def placejson(rf):
-    return {
-        'name': rf['장소명'].values[0] if not pd.isna(rf['장소명'].values[0]) else None,
-        'img': rf['이미지주소'].values[0] if not pd.isna(rf['이미지주소'].values[0]) else None,
-        'top': rf['위쪽장소'].values[0] if not pd.isna(rf['위쪽장소'].values[0]) else None,
-        'bottom': rf['아랫쪽장소'].values[0] if not pd.isna(rf['아랫쪽장소'].values[0]) else None,
-        'left': rf['왼쪽장소'].values[0] if not pd.isna(rf['왼쪽장소'].values[0]) else None,
-        'right': rf['오른쪽장소'].values[0] if not pd.isna(rf['오른쪽장소'].values[0]) else None,
-    }
-@app.post("/place")
-async def place(lendmark:str=Form(...)):
-    rf=navi[navi['시작점']&(navi['관광지명']==lendmark)]
-    return placejson(rf)
-@app.post("/clicknavi")
-async def place(name:str=Form(...)):
-    rf=navi[navi['장소명']==name]
-    return placejson(rf)
-if __name__=="__main__":
-    import uvicorn
-    uvicorn.run(app,host="0.0.0.0",port=8569,ssl_keyfile="key.pem",ssl_certfile="cert.pem",reload=True)
-#uvicorn main:app --host 0.0.0.0 --port 8569 --ssl-keyfile key.pem --ssl-certfile cert.pem --reload
+@app.post("/item")
+def yujeock(name:str=Form(...)):
+    tf=df[df['이름']==name].copy()
+    tf.rename(columns={'이름':'name','설명':'info'},inplace=True)
+    return tf.to_dict('records')
