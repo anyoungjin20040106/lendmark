a40da8521a3c69b917c7ca72039e6404db566e0b
안영진 <anyoungjin0106@kbu.ac.kr>
2024-10-02 21:51:20 +0900

    change main.py

--- main.py
+++ main.py
@@ -21,13 +21,28 @@ async def content(title: str = Form(...)):
     return item[item['이름'] == title]['설명'].values[0]
 
 @app.get("/map")
-async def map(lat:str=Query(...),lon:str=Query(...)):
+async def map(lat:str=Query(...),lon:str=Query(...),kind:str=Query(...)):
+    kind="" if kind=="모두보기" else kind
     m = folium.Map(location=[lat,lon], zoom_start=12)
     tf = lendmarkdf.copy()
+    tf=tf[tf['종류'].str.startswith(kind)]
     folium.Marker([lat,lon]).add_to(m)
     for row in tf.values:
         folium.Marker(location=[row[1],row[2]],icon=folium.Icon(color='red'),popup=f"""
-                      <h1>{row[0]}</h1><dialog id={row[0]}><form method="dialog"><h1>{row[0]}</h1><img src="{row[4]}" width="100%"><br><h2>{row[5]}</h2><button type="button"><h2>ar로 보기</h2></button><button><h2>닫기</h2></button></form></dialog><button onclick="document.getElementById('{row[0]}').showModal();">설명보기</button>""").add_to(m)
+                      <h1>{row[0]}</h1><dialog id={row[0]}><form method="dialog"><h1>{row[0]}</h1><img src="{row[4]}" width="100%"><br><h2>{row[5]}</h2><button><h2>닫기</h2></button></form></dialog><button onclick="document.getElementById('{row[0]}').showModal();">설명보기</button>""").add_to(m)
+    map_html = m._repr_html_()
+    return HTMLResponse(content=map_html)
+
+@app.get("/around")
+async def Around(lat:str=Query(...),lon:str=Query(...),kind:str=Query(...)):
+    kind="" if kind=="모두보기" else kind
+    m = folium.Map(location=[lat,lon], zoom_start=12)
+    tf = lendmarkdf.copy()
+    tf=tf[tf['종류'].str.startswith(kind)]
+    folium.Marker([lat,lon]).add_to(m)
+    for row in tf.values:
+        folium.Marker(location=[row[1],row[2]],icon=folium.Icon(color='red'),popup=f"""
+                      <h1>{row[0]}</h1><dialog id={row[0]}><form method="dialog"><h1>{row[0]}</h1><img src="{row[4]}" width="100%"><br><h2>{row[5]}</h2><button><h2>닫기</h2></button></form></dialog><button onclick="document.getElementById('{row[0]}').showModal();">설명보기</button>""").add_to(m)
     map_html = m._repr_html_()
     return HTMLResponse(content=map_html)
 @app.get("/kind")
